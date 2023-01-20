d81565339d0ceee8decde3d7e387ad7b1f521317
alex <alexisoro99@gmail.com>
2023-01-19 17:12:29 -0800

    webWorks Exercise

---
+++ app.js
@@ -0,0 +1,43 @@
+// Solidify Terminology
+// In your own terms, define the following terms:
+
+
+// 1. What is HTTP?
+
+    // HyperText Transfer Protocol the way I thought it worked was that it accepts a request from the internet and in return it'll contact that particular server and respond with what it can give, whether it be with the correct web page or an error code.
+
+// 2. What is a URL?
+
+    // The url is a web page address that locates and retrieves a specific page. It holds all the components a page would need like the protocol, domain and details in the webpage like the language.
+
+// 3. What is DNS?
+
+    // The Domain Name System translates what the user searches up and turns that into the correct ip address.
+
+// 4. What is a query string?
+
+    // It is the last part at the end of the url address and sometimes it is optional. Basically it gives extra information to a webpage mostly it could contain the language the web page is in or have the particular page with in the web page that it is refering too. 
+
+// 5. What are two HTTP verbs and how are they different?
+
+    // Two I can name are Get and Post. Get is what usually is being done when the user is accessing a web page it is essentially getting the information that it is allowed to get. Post is requesting the webpage to be able to add content into it, it is usually unallowed.
+    
+// 6. What is an HTTP request?
+
+    // It is the request from the user to HTTP which it is then their job to find the web page it needs. Usually the request is a Get method
+
+// 7. What is an HTTP response?
+
+    // This is the process HTTP does when sending out information to the user. Usually after the request the response is what follows. 
+
+// 8. What is an HTTP header? Give a couple examples of request and response headers you have seen.
+
+    // The header is extra information when it is in the proces of requesting or responding. Some examples for these headers could be: host. Cookies, cashe, and Last modified.
+
+// 9. What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+
+    // 1. When I entered the link the DNS turned it into a IP address so it can find it or something similar. 
+    // 2. My computer then makes a request with that IP address to other servers. 
+    // 3. The server sends me back a response, usually with a html web page in my case it was a Warning for dangerous malware. 
+    // 4. The html is put into a DOM made by the browser and takes into account images, CSS details, and other JavaScript. 
+    // 5. The browser then saves some information in the computer cache or router, and HTTP continues to request for any other information the web adddress might need.  
